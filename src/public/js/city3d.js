74bdd8f574d7ef62be10afb4c2a610c3ba887b6c
Alex Botelho <alexbotelho1@hotmail.com>
2020-04-30 20:30:41 -0300

    Site 1 feito em Express

--- src/public/js/city3d.js
+++ src/public/js/city3d.js
@@ -1,6 +1,16 @@
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
+const camera = new THREE.PerspectiveCamera( 
+   50, // Foco
+   window.innerWidth / window.innerHeight, // Proporção da imagem da câmera
+   1, 
+   5000 // Tamanho do mapa
+)
+camera.position.set( 
+   2000, //fundo (-) arco pro lado direito (+)
+   2000, // fundo (-) arco pra cima (+) 
+   2000, // Frente (-) e Costa (+)
+)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
@@ -16,26 +26,36 @@ window.addEventListener('resize', function() {
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
 
-// Cria a imagem do objeto, coloca a cor e a textura na imagem
-const geometry = new THREE.BoxGeometry( 1, 1, 1 )
+// // Cria a imagem do objeto, coloca a cor e a textura na imagem
+const geometry = new THREE.BoxGeometry( 10, 250, 250 )
 const cubeMaterials = [
    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } ),
    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } ),
-   new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } ),
-   new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } ),
-   new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } ),
-   new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('public/img/cubo.jpg'), side: THREE.DoubleSide } )
+   new THREE.MeshBasicMaterial( { color: 000000, side: THREE.DoubleSide } ),
+   new THREE.MeshBasicMaterial( { color: 000000, side: THREE.DoubleSide } ),
+   new THREE.MeshBasicMaterial( { color: 000000, side: THREE.DoubleSide } ),
+   new THREE.MeshBasicMaterial( { color: 000000, side: THREE.DoubleSide } )
 ]
 const material = new THREE.MeshFaceMaterial( cubeMaterials )
 const objeto = new THREE.Mesh( geometry, material )
+objeto.position.set(-450, 300, -450)
 scene.add( objeto )
 
-camera.position.set( 0, 0, 2 );
-controls.update();
+const iluminacao = new THREE.AmbientLight( 0xffffff, 0.9)
+scene.add( iluminacao )
+
+const loader = new THREE.GLTFLoader()
+const fileCity = 'public/img/city/scene.gltf'
+loader.load(
+   fileCity,
+   function ( objeto  ) {
+      scene.add( objeto.scene )
+   }
+)
 
 const update = () => {
-   objeto.rotation.x += 0.01;
-   objeto.rotation.y += 0.005;
+   // objeto.rotation.x += 0.01;
+   // objeto.rotation.y += 0.005;
 }
 
 const render = () => {
